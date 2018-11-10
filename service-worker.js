8ffd23b89eb56b4b688e67efb465709a0aab24f2
Thomas Maguire <thomas@maguire.email>
2018-11-10 01:29:58 +0000

    Archive subsite
    Completed migration of previous festivals section (from /gallery to archive.html and individual folders for each year instead of each festival.
    
    Updates to Service Worker and inclusion of Font Awesome (local) for WebApp.

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var cacheVersion = 8;
+var cacheVersion = 9;
 var currentCache = {
   offline: 'offline-cache' + cacheVersion
 };
@@ -9,10 +9,12 @@ const offlineUrl = 'offline-page.html';
 this.addEventListener('install', event => {
   event.waitUntil(
     caches.open(currentCache.offline).then(function(cache) {
+      //Contains assets used for app - when site is offline it will redirect to webapp instead of current offline page in future release
       return cache.addAll([
         'https://cbcf.info/css/bootstrap.css',
         'https://cbcf.info/css/mdb.css',
         'https://cbcf.info/css/style.css',
+        'https://cbcf.info/css/all.css',
         'https://cbcf.info/js/jquery.min.js',
         'https://cbcf.info/js/popper.min.js',
         'https://cbcf.info/js/bootstrap.js',
@@ -21,8 +23,28 @@ this.addEventListener('install', event => {
         'https://cbcf.info/js/webp.js',
         'https://cbcf.info/img/home/winter.webp',
         'https://cbcf.info/img/home/winter.png',
+        'https://cbcf.info/webfonts/fa-brands-400.eot',
+        'https://cbcf.info/webfonts/fa-brands-400.svg',
+        'https://cbcf.info/webfonts/fa-brands-400.ttf',
+        'https://cbcf.info/webfonts/fa-brands-400.woff',
+        'https://cbcf.info/webfonts/fa-brands-400.woff2',
+        'https://cbcf.info/webfonts/fa-light-300.eot',
+        'https://cbcf.info/webfonts/fa-light-300.svg',
+        'https://cbcf.info/webfonts/fa-light-300.ttf',
+        'https://cbcf.info/webfonts/fa-light-300.woff',
+        'https://cbcf.info/webfonts/fa-light-300.woff2',
+        'https://cbcf.info/webfonts/fa-regular-400.eot',
+        'https://cbcf.info/webfonts/fa-regular-400.svg',
+        'https://cbcf.info/webfonts/fa-regular-400.ttf',
+        'https://cbcf.info/webfonts/fa-regular-400.woff',
+        'https://cbcf.info/webfonts/fa-regular-400.woff2',
+        'https://cbcf.info/webfonts/fa-solid-900.eot',
+        'https://cbcf.info/webfonts/fa-solid-900.svg',
+        'https://cbcf.info/webfonts/fa-solid-900.ttf',
+        'https://cbcf.info/webfonts/fa-solid-900.woff',
+        'https://cbcf.info/webfonts/fa-solid-900.woff2'
         offlineUrl
-      ]);
+      ]); //Need to add additional pages/assets if required for datatable drinks list
     })
   );
 });
